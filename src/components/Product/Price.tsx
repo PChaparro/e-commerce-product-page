b81af61a05a3f32a7b2259699b46cbbbd236f579
Pedro Andrés Chaparro Quintero <pedroandreschaparro@gmail.com>
2023-07-09 17:20:34 -0500

    refactor: Move the `getFormattedPrice` to the `utils` folder

--- src/components/Product/Price.tsx
+++ src/components/Product/Price.tsx
@@ -1,3 +1,5 @@
+import { getFormattedPrice } from "../../utils/functions";
+
 interface PriceProps {
   price: number;
   discount?: number;
@@ -5,13 +7,6 @@ interface PriceProps {
 
 export const Price = ({ price, discount = 0 }: PriceProps) => {
   const finalPrice = price - (price * discount) / 100;
-
-  const getFormattedPrice = (price: number): string => {
-    return price.toLocaleString(undefined, {
-      minimumFractionDigits: 2,
-      maximumFractionDigits: 2,
-    });
-  };
 
   return (
     <div className="flex items-center justify-between gap-4 md:flex-col md:items-start md:gap-2">
---
+++ src/utils/functions.ts
@@ -0,0 +1,6 @@
+export const getFormattedPrice = (price: number): string => {
+  return price.toLocaleString(undefined, {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+};
