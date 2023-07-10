90c8e0b2cd2ce1b1970d73884a69f7dc0d1c15ec
Pedro Andrés Chaparro Quintero <pedroandreschaparro@gmail.com>
2023-07-09 20:21:54 -0500

    chore: Remove unused imports

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { carouselContext } from "../../context/CarouselContext";
 import { SlideButton } from "./SlideButton";
 import { Thumbnails } from "./Thumbnails";
